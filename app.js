8b8bfbd51c53fe080c0030f9d2b55ae4a66ed4ff
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-04-29 21:12:43 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//primitive veri tipleri -Karşılaştırılabilir
+
+let numOne = 3
+let numTwo = 3
+
+console.log(numOne == numTwo) // true
+
+let js = 'JavaScript'
+let py = 'Python'
+
+console.log(js == py) // false
+
+let lightOn = true
+let lightOff = false
+
+console.log(lightOn == lightOff) // false
+
+
