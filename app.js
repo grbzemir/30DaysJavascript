f797ab177211759b38b8321903e1c4b07123d315
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-04-29 21:27:42 +0300

    second commit

--- app.js
+++ app.js
@@ -16,3 +16,39 @@ let lightOff = false
 console.log(lightOn == lightOff) // false
 
 
+let nums = [1, 'prototurk', 3]
+
+// nums[0] // 1
+// nums[1] // prototurk
+// nums[2] // 3
+
+console.log(nums);
+
+nums[0] = 5;
+console.log(nums);
+
+//Dizileri fonksiyonları nesneleri karsılastırmayın
+
+let nums1 = [1, 2, 3]
+//let nums2 = [1, 2, 3]
+
+let nums2 = nums1
+
+console.log(nums1 == nums2) // false
+
+let user1 = {
+
+    name: 'emir',
+    surname: 'gürbüz',
+
+}
+
+let user2 = {
+
+    name: 'emir',
+    surname: 'gürbüz',
+
+}
+
+console.log(user1 == user2) // false
+
