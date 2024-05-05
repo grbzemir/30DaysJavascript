df4c55f5808e4d10eb99e5c376c9ea7c52bf80d6
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-06 02:53:06 +0300

    first commit

---
+++ Day23.js
@@ -0,0 +1,20 @@
+const button = document.getElementById('btn')
+
+
+button.addEventListener('mouseenter', function (e) {
+
+    console.log('butona tikladin!');
+
+})
+
+
+// function clickHandle() {
+//     console.log(e.getAttribute('id'))
+// }
+
+
+// const button = document.getElementById('btn')
+
+// button.onclick = e => {
+//     console.log(e)
+// }
