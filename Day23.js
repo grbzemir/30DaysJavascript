5dd9652f0f2b8f0bd381b4012860f5064d36f9f1
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-06 03:16:21 +0300

    first commit

--- Day23.js
+++ Day23.js
@@ -3,9 +3,25 @@ const button = document.getElementById('btn')
 
 button.addEventListener('mouseenter', function (e) {
 
-    console.log('butona tikladin!');
+    console.log(e);
 
 })
+
+const img = document.querySelector('img')
+img.style.opacity = 0;
+img.addEventListener('load', function () {
+    console.log('görsel yüklendi');
+    img.style.opactiy = 1;
+})
+
+img.addEventListener('error', function (e) {
+    console.log('resim yüklenirken bir sorun olustu', e);
+
+    //    e.target.src = ' ./no-image.png'
+
+})
+
+
 
 
 // function clickHandle() {
@@ -18,3 +34,40 @@ button.addEventListener('mouseenter', function (e) {
 // button.onclick = e => {
 //     console.log(e)
 // }
+
+
+const input = document.getElementById('name')
+
+input.addEventListener('input', e => {
+    console.log('Birseyler yaziliyor...', e.target.value)
+})
+
+
+const colorInput = document.getElementById('color-picker')
+
+colorInput.addEventListener('input', e => {
+
+    document.body.style.backgroundColor = e.target.value
+    console.log([...e, target.selectedOptions].map(el => el.value))
+})
+
+
+const saveButton = document.getElementById('save-btn')
+
+saveButton.addEventListener('click', e => {
+    console.log(input.value,
+        colorInput.value,
+        saveRulesCheckBox.checked
+    )
+})
+
+const genderSelect = document.getElementById('gender')
+
+genderSelect.addEventListener('change', e => {
+
+    console.log(e.target.selected)
+}
+)
+
+const saveRulesCheckBox = document.getElementById('save-rules')
+
